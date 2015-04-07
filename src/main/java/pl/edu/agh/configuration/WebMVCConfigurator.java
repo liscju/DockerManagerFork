21860a0f1e5de7863fde665b893ac7de79f09d01
liscju <piotr.listkiewicz@gmail.com>
2015-04-07 22:50:28 +0200

    Added basic embedded database support in app

--- src/main/java/pl/edu/agh/configuration/WebMVCConfigurator.java
+++ src/main/java/pl/edu/agh/configuration/WebMVCConfigurator.java
@@ -4,12 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
@@ -24,6 +28,17 @@ public class WebMVCConfigurator extends WebMvcConfigurerAdapter {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSource bean = new EmbeddedDatabaseBuilder()
+                .setName("DockerManagerDB")
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("db/schema.sql")
+                .addScript("db/initial_users.sql")
+                .build();
+        return bean;
     }
 
     @Override
