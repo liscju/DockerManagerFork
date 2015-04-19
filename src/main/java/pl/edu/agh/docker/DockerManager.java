84a52b7e8a9bca06bf29e0a5a930aab1d8af9239
Bukowski <bukowski@student.agh.edu.pl>
2015-04-19 17:21:51 +0200

    basic docker control

---
+++ src/main/java/pl/edu/agh/docker/DockerManager.java
@@ -0,0 +1,53 @@
+package pl.edu.agh.docker;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Info;
+import com.github.dockerjava.api.model.SearchItem;
+import com.github.dockerjava.api.model.Volume;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+
+public class DockerManager {
+	
+	private DockerClient dockerClient;
+	private String instanceAddress;
+	private List<CreateContainerResponse> containers;
+	
+	public DockerManager(String address){
+		instanceAddress=address;
+		dockerClient=DockerClientBuilder.getInstance(instanceAddress).build();
+	}
+	
+	
+	public Info getInfo(){
+		return dockerClient.infoCmd().exec();
+	}
+	
+	public List<SearchItem> searchForImage(String name){
+		return dockerClient.searchImagesCmd(name).exec();
+	}
+	
+	public CreateContainerResponse  createContainer(String name,String[] commands,Volume[] volumes){
+		CreateContainerResponse newContainer=dockerClient.createContainerCmd(name).withCmd(commands).withVolumes(volumes).exec();
+		containers.add(newContainer);
+		return newContainer;
+	}
+	
+	public void startContainer(CreateContainerResponse container){
+		dockerClient.startContainerCmd(container.getId()).exec();
+	}
+	
+	public void stopContainer(CreateContainerResponse container){
+		dockerClient.stopContainerCmd(container.getId()).exec();
+	}
+	
+	public void buildContainer(String dockerfile){
+		InputStream stream = new ByteArrayInputStream(dockerfile.getBytes());
+		dockerClient.buildImageCmd(stream).exec();
+	}
+
+}
