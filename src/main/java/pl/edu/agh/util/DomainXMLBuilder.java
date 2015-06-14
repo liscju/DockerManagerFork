8168b69b1b88e4989248cd5d345d7e2644cd528e
Bukowski <bukowski@student.agh.edu.pl>
2015-06-14 13:46:41 +0200

    alternate method of building xml

--- src/main/java/pl/edu/agh/util/DomainXMLBuilder.java
+++ src/main/java/pl/edu/agh/util/DomainXMLBuilder.java
@@ -1,5 +1,13 @@
 package pl.edu.agh.util;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class DomainXMLBuilder {
     private final String DOMAIN_XML_TEMPLATE =
             "<domain type='qemu'>\n" +
@@ -78,5 +86,77 @@ public class DomainXMLBuilder {
         generatedXML = generatedXML.replaceFirst(SOURCEFILE_PATTERN, sourceFile);
 
         return generatedXML;
+    }
+    
+    
+    public String createXML(String name,String memory,String cpu,String emulator,String source) throws ParserConfigurationException{
+    	DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		
+		Document doc = docBuilder.newDocument();
+		Element rootElement = doc.createElement("domain");
+		rootElement.setAttribute("type", "qemu");
+		doc.appendChild(rootElement);
+		
+		Element vmname = doc.createElement("name");
+		vmname.appendChild(doc.createTextNode(name));
+		rootElement.appendChild(vmname);
+		
+		Element uuid = doc.createElement("uuid");
+		rootElement.appendChild(uuid);
+		
+		Element vmem = doc.createElement("memory");
+		vmem.appendChild(doc.createTextNode(memory));
+		rootElement.appendChild(vmem);
+		
+		Element vcpu = doc.createElement("vcpu");
+		vcpu.appendChild(doc.createTextNode(cpu));
+		rootElement.appendChild(vcpu);
+		
+		Element os = doc.createElement("os");
+		rootElement.appendChild(os);
+		
+		Element ostype = doc.createElement("type");
+		ostype.setAttribute("arch", "x86_64");
+		ostype.setAttribute("machine", "pc");
+		ostype.appendChild(doc.createTextNode("hvm"));
+		os.appendChild(vcpu);
+		
+		
+		Element dev = doc.createElement("devices");
+		rootElement.appendChild(dev);
+		
+		Element emu = doc.createElement("emulator");
+		emu.appendChild(doc.createTextNode(emulator));
+		dev.appendChild(emu);
+		
+		Element disk = doc.createElement("disk");
+		disk.setAttribute("type", "file");
+		disk.setAttribute("device", "disk");
+		dev.appendChild(disk);
+		
+		Element sourcef = doc.createElement("source");
+		sourcef.setAttribute("file", source);
+		disk.appendChild(sourcef);
+		
+		Element target = doc.createElement("target");
+		target.setAttribute("dev", "hda");
+		disk.appendChild(target);
+		
+		Element nic = doc.createElement("interface");
+		nic.setAttribute("type", "network");
+		dev.appendChild(nic);
+		
+		Element nicsource = doc.createElement("source");
+		nicsource.setAttribute("network", "default");
+		nic.appendChild(target);
+		
+		Element vnc = doc.createElement("graphics");
+		vnc.setAttribute("type", "vnc");
+		vnc.setAttribute("port","-1");
+		dev.appendChild(vnc);
+		
+		
+		return doc.toString();
     }
 }
