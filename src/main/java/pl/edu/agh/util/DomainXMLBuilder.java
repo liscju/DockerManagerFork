fb574e960d6f0733d522bd15cbf9c779477d51e0
liscju <piotr.listkiewicz@gmail.com>
2015-06-15 12:35:08 +0200

    Added simple unit test for DomainXML Builder, fix layouts in code

--- src/main/java/pl/edu/agh/util/DomainXMLBuilder.java
+++ src/main/java/pl/edu/agh/util/DomainXMLBuilder.java
@@ -17,37 +17,10 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class DomainXMLBuilder {
-    private final String DOMAIN_XML_TEMPLATE =
-            "<domain type='qemu'>\n" +
-            "  <name>%domain_name%</name>\n" +
-            "  <uuid></uuid>\n" +
-            "  <memory>%domain_memory%</memory>\n" +
-            "  <vcpu>%domain_vcpu%</vcpu>\n" +
-            "  <os>\n" +
-            "    <type arch='i686' machine='pc'>hvm</type>\n" +
-            "  </os>\n" +
-            "  <devices>\n" +
-            "    <emulator>%domain_emulator%</emulator>\n" +
-            "    <disk type='file' device='disk'>\n" +
-            "      <source file='%domain_sourcefile%'/>\n" +
-            "      <target dev='hda'/>\n" +
-            "    </disk>\n" +
-            "    <interface type='network'>\n" +
-            "      <source network='default'/>\n" +
-            "    </interface>\n" +
-            "    <graphics type='vnc' port='-1'/>\n" +
-            "  </devices>\n" +
-            "</domain>";
-
-    private final String NAME_PATTERN = "%domain_name%";
-    private final String MEMORY_PATTERN = "%domain_memory%";
-    private final String VPU_PATTERN = "%domain_vcpu%";
-    private final String EMULATOR_PATTERN = "%domain_emulator%";
-    private final String SOURCEFILE_PATTERN = "%domain_sourcefile%";
 
     private String name = null;
-    private String memory = "100000";
-    private String vcpu = "1";
+    private String memory = null;
+    private String vcpu = null;
     private String emulator = null;
     private String sourceFile = null;
 
@@ -82,18 +55,26 @@ public class DomainXMLBuilder {
     }
 
     public String buildXML() {
-        if (name == null || emulator == null || sourceFile == null) {
-            throw new IllegalArgumentException("You must supply name,emulator and source file");
+        if (name == null) {
+            throw new IllegalArgumentException("You must supply name of domain");
         }
 
-        String generatedXML = new String(DOMAIN_XML_TEMPLATE);
-        generatedXML = generatedXML.replaceFirst(NAME_PATTERN, name);
-        generatedXML = generatedXML.replaceFirst(MEMORY_PATTERN,memory);
-        generatedXML = generatedXML.replaceFirst(VPU_PATTERN,vcpu);
-        generatedXML = generatedXML.replaceFirst(EMULATOR_PATTERN,emulator);
-        generatedXML = generatedXML.replaceFirst(SOURCEFILE_PATTERN, sourceFile);
+        if (memory == null) {
+            throw new IllegalArgumentException("You must supply amount of memory");
+        }
 
-        //return generatedXML;
+        if (vcpu == null) {
+            throw new IllegalArgumentException("You must supply amount of vcpu of domain");
+        }
+        
+        if (emulator == null) {
+            throw new IllegalArgumentException("You must supply emulator to create domain");
+        }
+
+        if (sourceFile == null) {
+            throw new IllegalArgumentException("You must supply source file of the vm");
+        }
+
         return createXML(name,memory,vcpu,emulator,sourceFile);
     }
     
@@ -105,78 +86,77 @@ public class DomainXMLBuilder {
 		try {
 			docBuilder = docFactory.newDocumentBuilder();
 
-		
-		Document doc = docBuilder.newDocument();
-		Element rootElement = doc.createElement("domain");
-		rootElement.setAttribute("type", "qemu");
-		doc.appendChild(rootElement);
-		
-		Element vmname = doc.createElement("name");
-		vmname.appendChild(doc.createTextNode(name));
-		rootElement.appendChild(vmname);
-		
-		Element uuid = doc.createElement("uuid");
-		rootElement.appendChild(uuid);
-		
-		Element vmem = doc.createElement("memory");
-		vmem.appendChild(doc.createTextNode(memory));
-		rootElement.appendChild(vmem);
-		
-		Element vcpu = doc.createElement("vcpu");
-		vcpu.appendChild(doc.createTextNode(cpu));
-		rootElement.appendChild(vcpu);
-		
-		Element os = doc.createElement("os");
-		rootElement.appendChild(os);
-		
-		Element ostype = doc.createElement("type");
-		ostype.setAttribute("arch", "x86_64");
-		ostype.setAttribute("machine", "pc");
-		ostype.appendChild(doc.createTextNode("hvm"));
-		os.appendChild(ostype);
-		
-		
-		Element dev = doc.createElement("devices");
-		rootElement.appendChild(dev);
-		
-		Element emu = doc.createElement("emulator");
-		emu.appendChild(doc.createTextNode(emulator));
-		dev.appendChild(emu);
-		
-		Element disk = doc.createElement("disk");
-		disk.setAttribute("type", "file");
-		disk.setAttribute("device", "disk");
-		dev.appendChild(disk);
-		
-		Element sourcef = doc.createElement("source");
-		sourcef.setAttribute("file", source);
-		disk.appendChild(sourcef);
-		
-		Element target = doc.createElement("target");
-		target.setAttribute("dev", "hdc");
-		disk.appendChild(target);
-		
-		Element nic = doc.createElement("interface");
-		nic.setAttribute("type", "network");
-		dev.appendChild(nic);
-		
-		Element nicsource = doc.createElement("source");
-		nicsource.setAttribute("network", "default");
-		nic.appendChild(target);
-		
-		Element vnc = doc.createElement("graphics");
-		vnc.setAttribute("type", "vnc");
-		vnc.setAttribute("port","-1");
-		dev.appendChild(vnc);
-		
-	    TransformerFactory tFact = TransformerFactory.newInstance();
-        Transformer trans = tFact.newTransformer();
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("domain");
+            rootElement.setAttribute("type", "qemu");
+            doc.appendChild(rootElement);
 
-        StringWriter writer = new StringWriter();
-        StreamResult result = new StreamResult(writer);
-        DOMSource sourcex = new DOMSource(doc);
-        trans.transform(sourcex, result);
-        res=writer.toString();
+            Element vmname = doc.createElement("name");
+            vmname.appendChild(doc.createTextNode(name));
+            rootElement.appendChild(vmname);
+
+            Element uuid = doc.createElement("uuid");
+            rootElement.appendChild(uuid);
+
+            Element vmem = doc.createElement("memory");
+            vmem.appendChild(doc.createTextNode(memory));
+            rootElement.appendChild(vmem);
+
+            Element vcpu = doc.createElement("vcpu");
+            vcpu.appendChild(doc.createTextNode(cpu));
+            rootElement.appendChild(vcpu);
+
+            Element os = doc.createElement("os");
+            rootElement.appendChild(os);
+
+            Element ostype = doc.createElement("type");
+            ostype.setAttribute("arch", "x86_64");
+            ostype.setAttribute("machine", "pc");
+            ostype.appendChild(doc.createTextNode("hvm"));
+            os.appendChild(ostype);
+
+
+            Element dev = doc.createElement("devices");
+            rootElement.appendChild(dev);
+
+            Element emu = doc.createElement("emulator");
+            emu.appendChild(doc.createTextNode(emulator));
+            dev.appendChild(emu);
+
+            Element disk = doc.createElement("disk");
+            disk.setAttribute("type", "file");
+            disk.setAttribute("device", "disk");
+            dev.appendChild(disk);
+
+            Element sourcef = doc.createElement("source");
+            sourcef.setAttribute("file", source);
+            disk.appendChild(sourcef);
+
+            Element target = doc.createElement("target");
+            target.setAttribute("dev", "hdc");
+            disk.appendChild(target);
+
+            Element nic = doc.createElement("interface");
+            nic.setAttribute("type", "network");
+            dev.appendChild(nic);
+
+            Element nicsource = doc.createElement("source");
+            nicsource.setAttribute("network", "default");
+            nic.appendChild(target);
+
+            Element vnc = doc.createElement("graphics");
+            vnc.setAttribute("type", "vnc");
+            vnc.setAttribute("port","-1");
+            dev.appendChild(vnc);
+
+            TransformerFactory tFact = TransformerFactory.newInstance();
+            Transformer trans = tFact.newTransformer();
+
+            StringWriter writer = new StringWriter();
+            StreamResult result = new StreamResult(writer);
+            DOMSource sourcex = new DOMSource(doc);
+            trans.transform(sourcex, result);
+            res=writer.toString();
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		} catch (TransformerConfigurationException e) {
@@ -184,7 +164,6 @@ public class DomainXMLBuilder {
 		} catch (TransformerException e) {
 			e.printStackTrace();
 		}
-		
 		
 		return res;
     }
---
+++ src/test/java/pl/edu/agh/util/DomainXMLBuilderTest.java
@@ -0,0 +1,102 @@
+package pl.edu.agh.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DomainXMLBuilderTest {
+
+    private final static String ANY_DOMAIN = "DOMAIN";
+    private final static int ANY_MEMORY = 200;
+    private final static int ANY_VCPU = 1;
+    private final static String ANY_EMULATOR = "/usr/bin/quemu";
+    private final static String ANY_SOURCE = "/home/images/REact.vmdk";
+
+    @Test
+    public void testShouldThrowExceptionWhenUserNotSuppliedName() throws Exception {
+        DomainXMLBuilder domainXMLBuilder = new DomainXMLBuilder();
+        try {
+            String xml = domainXMLBuilder.buildXML();
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("You must supply name of domain",e.getMessage());
+        }
+    }
+
+    @Test
+    public void testShouldThrowExceptionWhenUserNotSuppliedMemory() throws Exception {
+        DomainXMLBuilder domainXMLBuilder = new DomainXMLBuilder();
+        try {
+            String xml = domainXMLBuilder
+                    .withName(ANY_DOMAIN)
+                    .buildXML();
+
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("You must supply amount of memory",e.getMessage());
+        }
+    }
+
+    @Test
+    public void testShouldThrowExceptionWhenUserNotSuppliedVcpu() throws Exception {
+        DomainXMLBuilder domainXMLBuilder = new DomainXMLBuilder();
+        try {
+            String xml = domainXMLBuilder
+                    .withName(ANY_DOMAIN)
+                    .withMemory(ANY_MEMORY)
+                    .buildXML();
+
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("You must supply amount of vcpu of domain",e.getMessage());
+        }
+    }
+
+    @Test
+    public void testShouldThrowExceptionWhenUserNotSuppliedEmulator() throws Exception {
+        DomainXMLBuilder domainXMLBuilder = new DomainXMLBuilder();
+        try {
+            String xml = domainXMLBuilder
+                    .withName(ANY_DOMAIN)
+                    .withMemory(ANY_MEMORY)
+                    .withVCpu(ANY_VCPU)
+                    .buildXML();
+
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("You must supply emulator to create domain",e.getMessage());
+        }
+    }
+
+    @Test
+    public void testShouldThrowExceptionWhenUserNotSuppliedSourceFile() throws Exception {
+        DomainXMLBuilder domainXMLBuilder = new DomainXMLBuilder();
+        try {
+            String xml = domainXMLBuilder
+                    .withName(ANY_DOMAIN)
+                    .withMemory(ANY_MEMORY)
+                    .withVCpu(ANY_VCPU)
+                    .withEmulator(ANY_EMULATOR)
+                    .buildXML();
+
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("You must supply source file of the vm",e.getMessage());
+        }
+    }
+
+    @Test
+    public void testForSimpleTestCase() throws Exception {
+        DomainXMLBuilder domainXMLBuilder = new DomainXMLBuilder();
+        String domainXML = domainXMLBuilder
+                .withName("ReactOS-on-QEMU")
+                .withMemory(100000)
+                .withVCpu(1)
+                .withEmulator("/usr/bin/qemu-system-x86_64")
+                .withSourceFile("/home/liscju/LibVirtExercises/images/ReactOS.vmdk")
+                .buildXML();
+
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><domain type=\"qemu\"><name>ReactOS-on-QEMU</name><uuid/><memory>100000</memory><vcpu>1</vcpu><os><type arch=\"x86_64\" machine=\"pc\">hvm</type></os><devices><emulator>/usr/bin/qemu-system-x86_64</emulator><disk device=\"disk\" type=\"file\"><source file=\"/home/liscju/LibVirtExercises/images/ReactOS.vmdk\"/></disk><interface type=\"network\"><target dev=\"hdc\"/></interface><graphics port=\"-1\" type=\"vnc\"/></devices></domain>",
+                domainXML);
+    }
+}
